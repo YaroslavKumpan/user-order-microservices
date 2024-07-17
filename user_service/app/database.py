c4e823d45428ad59ca457022bbb640b8f6280f86
Yaroslav Kumpan <yroslavkumpan@gmail.com>
2024-07-17 21:03:01 +0300

    create database.py

---
+++ user_service/app/database.py
@@ -0,0 +1,35 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+load_dotenv()
+DATABASE_URL = os.getenv("USER_SERVICE_DATABASE_URL")
+
+
+engine = create_engine(
+    DATABASE_URL, echo=True
+)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+
+def init_db():
+    Base.metadata.create_all(
+        bind=engine
+    )
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
